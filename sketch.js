20ab2c6fe9782f94d1dace11d7bcae677da0322f
CODE09876 <76430477+CODE09876@users.noreply.github.com>
2021-01-01 13:01:21 +0530

    update sketch.js

--- sketch.js
+++ sketch.js
@@ -40,7 +40,7 @@ function draw() {
 }
 function keyPressed(){
 	if (keyCode === UP_ARROW){
-		Matter.body.applyForce(ball.body,ball.body.position,{x:85,y: -85});
+		Matter.Body.applyForce(ball.body,ball.body.position,{x:85,y: -85});
 	}
 }
 
